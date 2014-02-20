0b22ebcea41ec61ee2126ac35cce08d9bcd2be13
Don Nelson <dnelson@gopivotal.com>
2014-02-19 17:47:41 -0800

    webui page, servlet, web.xml, ignore /bin

---
+++ webui/src/main/java/org/sc/CalcController.java
@@ -0,0 +1,46 @@
+package org.sc;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcController
+ */
+public class CalcController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sDate = request.getParameter("startDate");
+		String eDate = request.getParameter("endDate");
+		String region = request.getParameter("region");
+		String envType = request.getParameter("envType");
+		String numEnv = request.getParameter("envNum");
+		response.getWriter().print("start: " + sDate + " end: " + eDate + " region: " + region +
+				" Env type: " + envType + " # Env: " + numEnv);
+	}
+
+}
